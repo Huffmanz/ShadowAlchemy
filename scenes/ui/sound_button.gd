3ffbad8175729a9d7c986069da9adcd1b47ea60b
Huffmanz <huffmanz2012@gmail.com>
2024-07-28 23:13:40 -0500

    cover art, buttons

--- scenes/ui/sound_button.gd
+++ scenes/ui/sound_button.gd
@@ -2,9 +2,20 @@ class_name SoundButton
 extends Button
 
 @onready var random_stream_player = $RandomStreamPlayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hover_stream_player: RandomStreamPlayer = $HoverStreamPlayer
+
 
 func _ready():
 	pressed.connect(on_pressed)
+	mouse_entered.connect(on_mouse_entered)
+	pivot_offset = size / 2
 	
 func on_pressed():
 	random_stream_player.play_random()
+	
+func on_mouse_entered():
+	if disabled == true:
+		return
+	animation_player.play("hover")
+	hover_stream_player.play_random()
